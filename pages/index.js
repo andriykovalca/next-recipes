17fe33aca5b74d4b08c28afeef86d48e05d8b0a5
andriykovalca <akovalwm@gmail.com>
2022-01-06 20:19:44 -0500

    contentful integration

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,64 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import React from 'react'
 import styles from '../styles/Home.module.css'
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-
-
-export default function Home() {
-  
-  const router = useRouter();
-  const { postId } = router.query;
-  
-  const [posts, setPosts] = useState([]);
-  const [post, setPost] = useState({});
-
-  useEffect(() => {
-    if (postId) {
-      fetchPost(postId);
-    } else {
-      fetchPosts();
-      setPost({});
-    }
-  }, [postId]);
+import {getAllPostsForHome} from "../lib/api"
 
 
 
-  const baseUrl = 'https://api.edamam.com/api/recipes/v2?type=public'
-  const request = '&q='
-  const question = 'chicken'
-  const questiona = 'aaa'
-  const apiKey = process.env.RECIPE_API_KEY
-  const appId = process.env.RECIPE_APP_ID
-  
-  const fetchUrl = baseUrl + request + question + '&app_id=' + appId + '&app_key=' + apiKey
-
-  let fetchPosts = () => {
-    fetch('https://gorest.co.in/public/v1/posts')
-      .then((response) => response.json())
-      .then((response) => {
-        setPosts(response.data);
-      });
-  };
-
-  let fetchPost = (postId) => {
-    fetch(`https://gorest.co.in/public/v1/posts/${postId}`)
-      .then((response) => response.json())
-      .then((response) => {
-        setPost(response.data);
-      });
-  };
-
-  let appendQuery = (id) => {
-    router.query.postId = id;
-    router.push(router);
-  };
-
+export default function Home({allPosts}) {
   return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <div className={styles.buttonContainer}>
-<<<<<<< HEAD
-          <Link href="/">Home page</Link>
-=======
-          <Link href="/">Home page</Link>
->>>>>>> de836512b79f0873a5652312f5262925cd7fa47e
-        </div>
-        {posts.map((item) => {
-          return (
-            <div className={styles.card} onClick={() => appendQuery(item.id)}>
-              <div className={styles.number}>{item.id}</div>
-              <h2>{item.title}</h2>
-            </div>
-          );
-        })}
-      </div>
-      {post.id ? (
-        <div className={styles.right}>
-          <div className={styles.fixedCard}>
-            <div className={styles.number}>{post.id}</div>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
+      <div className={styles.container}>
+      <Head>
+          <title>Blog app</title>
+          <meta name="description" content="Simple blog app with Contentful CMS" />
+          <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+
+          <div className={styles.grid}>
+          {
+              allPosts.length > 0 ? (
+              allPosts.map((post) => (
+                  <div className={styles.card} key={post.sys.id}>
+                  <div className={styles.imageHolder}>
+                      <img src={post.coverImage.url} alt={post.title} />
+                  </div>
+                  <div className={styles.details}>
+                      <Link href={`posts/${post.sys.id}`}>
+                      <a>
+                      {post.title} &rarr;
+                      </a>
+                      </Link>
+                      <p>{post.excerpt}</p>
+                  </div>
+                  </div>
+              ))
+              ) : (
+              <div className={styles.card}>
+              <p>No posts added!</p>
           </div>
-        </div>
-      ) : null}
-    </div>
-  );
+              )
+          }
+          </div>
+      </main>
+
+      <footer className={styles.footer}>
+          <p>Simple blog app</p>
+      </footer>
+      </div>
+  )
+}
+
+
+export async function getStaticProps({preview = false}){
+  let allPosts = (await getAllPostsForHome(preview))  ?? [];
+
+  return {
+      props: { preview, allPosts }
+      
+  }
+  
 }
